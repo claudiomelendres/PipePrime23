739103273444febaee5d4fe0402148163d7c4abf
claudiomelendresnogales <claudio.melendres@incontact.com>
2023-09-23 11:22:05 -0400

    first message

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+
+
+
+
+@NgModule({
+  exports: [
+    MenuModule,
+    ButtonModule
+  ]
+})
+export class PrimeNGModule { }
