4b88ca9daced97cfadcd303d8fb1277602cf6ac7
claudiomelendres <claudiomelendres@gmail.com>
2023-09-27 19:53:26 -0400

    pipes custom

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { MenuModule } from 'primeng/menu';
 import { ButtonModule } from 'primeng/button';
-
-
-
+import { MenubarModule } from 'primeng/menubar';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @NgModule({
   exports: [
     MenuModule,
-    ButtonModule
-  ]
+    ButtonModule,
+    MenubarModule,
+    CardModule,
+    FieldsetModule,
+    PanelModule,
+    TableModule,
+    ToolbarModule,
+  ],
 })
-export class PrimeNGModule { }
+export class PrimeNGModule {}
